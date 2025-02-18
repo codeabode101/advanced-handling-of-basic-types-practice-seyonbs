2f1c9cb6b3f6eef0c302dcd6402fe33677aa62e8
seyonbs <seyonbs@gmail.com>
2025-02-17 19:06:34 -0500

    Add files via upload

---
+++ dive starter.py
@@ -0,0 +1,8 @@
+depth =200  
+current_depth = depth + 100 
+print("Depth:", current_depth, "meters")
+oxygen = input("Is your oxygen low(yes/no)")
+if oxygen == "yes":
+    print("emergancy air")
+if oxygen == "no":
+    print("oxygen levels are fine")      

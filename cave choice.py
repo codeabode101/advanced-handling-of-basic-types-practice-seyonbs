9ce4c2f6d9f8c50fc9960d8d35d1180e1fb80a8f
seyonbs <seyonbs@gmail.com>
2025-02-18 19:04:38 -0500

    Add files via upload

---
+++ cave choice.py
@@ -0,0 +1,8 @@
+cave_temp = 10  
+safe_temp = cave_temp - 2  
+print("Safe temp:", safe_temp)
+Two_tunnels_appear = input("Should we go left(yes/no)")
+if Two_tunnels_appear == "yes":
+    print("we should go left")
+if Two_tunnels_appear == "no":
+    print("we should not go left")      

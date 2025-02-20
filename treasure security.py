d743ac38323391389abe26a35a0496e0b7d41399
seyonbs <seyonbs@gmail.com>
2025-02-19 20:46:14 -0500

    Add files via upload

---
+++ treasure security.py
@@ -0,0 +1,9 @@
+code = int(input("Code? :")) 
+if code == 3:
+    print("Open!")
+else:
+    print("wrong")
+is_alarm_on = bool(input("is alarm on?"))
+if  is_alarm_on == "true":
+    print("Intruder alert!")
+             

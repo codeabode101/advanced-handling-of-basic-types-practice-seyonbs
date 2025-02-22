d21a2e4122149a43f55a5060fdaa7852a10e8e03
seyonbs <seyonbs@gmail.com>
2025-02-21 19:47:05 -0500

    Add files via upload

---
+++ emergancy escape.py
@@ -0,0 +1,9 @@
+pressure = 1500  
+if pressure > 1000: 
+    print("Leak detected!")
+is_leaking : bool
+is_leaking = input("seal doors(true/false)")
+if is_leaking == "true":
+    print("danger is not here.")
+else:
+    print("danger!danger!")          

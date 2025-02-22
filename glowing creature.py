6eaaefba3bf5a61607b04c5fb0107e6880f88ca4
seyonbs <seyonbs@gmail.com>
2025-02-21 19:35:15 -0500

    Add files via upload

---
+++ glowing creature.py
@@ -0,0 +1,9 @@
+health = 80  
+damage = 20  
+new_health = health - damage  
+print("Health:", new_health)
+a_jellyfish_glows = (input("shine light(yes/no)"))
+if a_jellyfish_glows == "yes":
+    print("jellyfish leaves.")
+else:
+    print("jellyfish stays.")        
